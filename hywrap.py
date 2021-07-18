41ea6845b061fd867377997a8460572ec0bfee5a
Supelion <83178953+Supelion@users.noreply.github.com>
2021-07-18 12:11:45 +0300

    HyWrap is now Asynchronous

--- hywrap.py
+++ hywrap.py
@@ -1,50 +1,86 @@
-import requests
+import aiohttp
 
-def bedwars(uuid : str, hypixelapikey : str = None):
+async def bedwars(uuid : str, hypixelapikey : str = None):
     if hypixelapikey is None:
         print("Please Enter a Hypixel API Key!")
+    if uuid is None:
+        print("Please specify a UUID!")
     else:
-        api = requests.get(f"https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}").json()
-        return api["player"]["stats"]["Bedwars"]
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f'https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}') as bedwarsRaw:
+                rawBW = await bedwarsRaw.json()
+                bedwarsStats = rawBW["player"]["stats"]["Bedwars"]
+                return bedwarsStats
 
-def skywars(uuid : str, hypixelapikey : str = None):
+async def skywars(uuid : str, hypixelapikey : str = None):
     if hypixelapikey is None:
         print("Please Enter a Hypixel API Key!")
+    if uuid is None:
+        print("Please specify a UUID!")
     else:
-        api = requests.get(f"https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}").json()
-        return api["player"]["stats"]["SkyWars"]
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f'https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}') as skywarsRaw:
+                rawSW = await skywarsRaw.json()
+                skywarsStats = rawSW["player"]["stats"]["SkyWars"]
+                return skywarsStats
 
-def duels(uuid : str, hypixelapikey : str = None):
+async def duels(uuid : str, hypixelapikey : str = None):
     if hypixelapikey is None:
         print("Please Enter a Hypixel API Key!")
+    if uuid is None:
+        print("Please specify a UUID!")
     else:
-        api = requests.get(f"https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}").json()
-        return api["player"]["stats"]["Duels"]
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f'https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}') as duelsRaw:
+                rawDuels = await duelsRaw.json()
+                duelsStats = rawDuels["player"]["stats"]["Duels"]
+                return duelsStats
 
-def arcade(uuid : str, hypixelapikey : str = None):
+async def arcade(uuid : str, hypixelapikey : str = None):
     if hypixelapikey is None:
         print("Please Enter a Hypixel API Key!")
+    if uuid is None:
+        print("Please specify a UUID!")
     else:
-        api = requests.get(f"https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}").json()
-        return api["player"]["stats"]["Arcade"]
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f'https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}') as arcadeRaw:
+                rawArcade = await arcadeRaw.json()
+                arcadeStats = rawArcade["player"]["stats"]["Arcade"]
+                return arcadeStats
 
-def buildBattle(uuid : str, hypixelapikey : str = None):
+
+async def buildBattle(uuid : str, hypixelapikey : str = None):
     if hypixelapikey is None:
         print("Please Enter a Hypixel API Key!")
+    if uuid is None:
+        print("Please specify a UUID!")
     else:
-        api = requests.get(f"https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}").json()
-        return api["player"]["stats"]["BuildBattle"]
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f'https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}') as buildRaw:
+                rawBuild = await buildRaw.json()
+                buildBattleStats = rawBuild["player"]["stats"]["BuildBattle"]
+                return buildBattleStats
 
-def pit(uuid : str, hypixelapikey : str = None):
+async def pit(uuid : str, hypixelapikey : str = None):
     if hypixelapikey is None:
         print("Please Enter a Hypixel API Key!")
+    if uuid is None:
+        print("Please specify a UUID!")
     else:
-        api = requests.get(f"https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}").json()
-        return api["player"]["stats"]["Pit"]
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f'https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}') as pitRaw:
+                rawPit = await pitRaw.json()
+                pitStats = rawPit["player"]["stats"]["Pit"]
+                return pitStats
 
-def skyblock(uuid : str, hypixelapikey : str = None):
+async def skyblock(uuid : str, hypixelapikey : str = None):
     if hypixelapikey is None:
         print("Please Enter a Hypixel API Key!")
+    if uuid is None:
+        print("Please specify a UUID!")
     else:
-        api = requests.get(f"https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}").json()
-        return api["player"]["stats"]["SkyBlock"]
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f'https://api.hypixel.net/player?key={hypixelapikey}&uuid={uuid}') as sbraw:
+                rawSB = await sbraw.json()
+                skyblockStats = rawSB["player"]["stats"]["SkyBlock"]
+                return skyblockStats
